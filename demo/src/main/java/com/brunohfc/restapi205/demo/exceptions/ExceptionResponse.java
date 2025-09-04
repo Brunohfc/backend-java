f054bc0bc10aa41752232fa11326b1546928162a
Bruno <Bhenrick8@gmail.com>
2025-09-04 17:13:13 -0300

    feat: commit inicial

--- demo/src/main/java/com/brunohfc/restapi205/demo/exceptions/ExceptionResponse.java
+++
@@ -1,7 +0,0 @@
-package com.brunohfc.restapi205.demo.exceptions;
-
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Date;
-
-public record ExceptionResponse(Date timestamp, String message, String details) {}

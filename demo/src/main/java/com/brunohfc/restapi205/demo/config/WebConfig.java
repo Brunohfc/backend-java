014861b318e7dc3aa01fb84a7eec4bb3e421423f
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-09-14 22:58:30 -0300

    feat: implementacao da mediaType para JSON e XML

---
+++ demo/src/main/java/com/brunohfc/restapi205/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.brunohfc.restapi205.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        configurer.
+                favorParameter(true).
+                parameterName("mediaType").
+                ignoreAcceptHeader(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("json", MediaType.APPLICATION_JSON);
+    }
+}
--- demo/src/main/java/com/brunohfc/restapi205/demo/controller/PersonController.java
+++ demo/src/main/java/com/brunohfc/restapi205/demo/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.brunohfc.restapi205.demo.data.dto.v1.PersonDTO;
 import com.brunohfc.restapi205.demo.data.dto.v2.PersonDTOV2;
 import com.brunohfc.restapi205.demo.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,36 +23,43 @@ public class PersonController {
         return "Funcionando";
     }
 
-    @GetMapping
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<PersonDTO> listPerson(){
         return personService.findAll();
     }
 
-    @PostMapping("/create")
+    @PostMapping(value = "/create",
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonDTO create(@RequestBody PersonDTO person){
         return personService.create(person);
 
 
     }
 
-    @PostMapping(value = "/v2/create")
+    @PostMapping(value = "/v2/create",
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
     public PersonDTOV2 create(@RequestBody PersonDTOV2 person){
         return personService.createv2(person);
 
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}" ,
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonDTO getPerson(@PathVariable Long id){
         return personService.getById(id);
     }
 
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+    produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonDTO update(@RequestBody PersonDTO person){
         return personService.update(person);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteById(@PathVariable Long id){
         personService.delete(id);
 

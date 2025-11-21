90b0620202922f9d8db8c517ad5e1a8c1d8582d0
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-11-21 18:41:24 -0300

    feat: funcionalidade de mandar email com anexo funcionando

--- demo/src/main/java/com/brunohfc/restapi205/demo/controller/EmailController.java
+++ demo/src/main/java/com/brunohfc/restapi205/demo/controller/EmailController.java
@@ -3,16 +3,20 @@ package com.brunohfc.restapi205.demo.controller;
 import com.brunohfc.restapi205.demo.controller.docs.EmailControllerDocs;
 import com.brunohfc.restapi205.demo.data.dto.request.EmailRequestDTO;
 import com.brunohfc.restapi205.demo.services.EmailService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.internet.AddressException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/email/v1")
@@ -22,16 +26,18 @@ public class EmailController implements EmailControllerDocs {
     @Autowired
     EmailService emailService;
 
-    @Override
     @PostMapping
+    @Override
     public ResponseEntity<String> sendEamil(@RequestBody EmailRequestDTO dto) throws AddressException {
         emailService.sendEmail(dto);
         return new ResponseEntity<>("email send with success", HttpStatus.OK);
     }
 
+    @PostMapping(value = "/attach", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Override
-    public ResponseEntity<String> sendEamilWithAttachment(@RequestBody String emailRequestJson, MultipartFile file) {
-
-        return null;
+    public ResponseEntity<String> sendEamilWithAttachment(@RequestParam("emailRequest") String emailRequest,
+                                                         @RequestParam("attach") MultipartFile file) {
+        emailService.sendEmailWithAttach(emailRequest, file);
+        return new ResponseEntity<>("email send with success", HttpStatus.OK);
     }
 }
--- demo/src/main/java/com/brunohfc/restapi205/demo/services/EmailService.java
+++ demo/src/main/java/com/brunohfc/restapi205/demo/services/EmailService.java
@@ -3,9 +3,16 @@ package com.brunohfc.restapi205.demo.services;
 import com.brunohfc.restapi205.demo.config.EmailConfig;
 import com.brunohfc.restapi205.demo.data.dto.request.EmailRequestDTO;
 import com.brunohfc.restapi205.demo.mail.EmailSender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.internet.AddressException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class EmailService {
@@ -20,5 +27,27 @@ public class EmailService {
                 .withSubject(dto.getSubject())
                 .withMessage(dto.getBody())
                 .sendEmail(emailConfig);
+    }
+
+    public void sendEmailWithAttach(String emailRequestJson, MultipartFile file){
+        File tempFile = null;
+
+        try {
+            EmailRequestDTO request = new ObjectMapper().readValue(emailRequestJson, EmailRequestDTO.class);
+            tempFile = File.createTempFile("file",file.getOriginalFilename());
+            file.transferTo(tempFile);
+
+            emailSender.to(request.getTo())
+                    .withSubject(request.getSubject())
+                    .withMessage(request.getBody())
+                    .withAttachment(tempFile.getAbsolutePath())
+                    .sendEmail(emailConfig);
+
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (AddressException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }

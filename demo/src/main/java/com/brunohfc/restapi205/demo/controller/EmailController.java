006a747ed96365da5298387d99bc020befbe1d71
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-11-23 11:38:30 -0300

    chore: organizando importacoes do EmailController.java

--- demo/src/main/java/com/brunohfc/restapi205/demo/controller/EmailController.java
+++ demo/src/main/java/com/brunohfc/restapi205/demo/controller/EmailController.java
@@ -3,20 +3,13 @@ package com.brunohfc.restapi205.demo.controller;
 import com.brunohfc.restapi205.demo.controller.docs.EmailControllerDocs;
 import com.brunohfc.restapi205.demo.data.dto.request.EmailRequestDTO;
 import com.brunohfc.restapi205.demo.services.EmailService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.internet.AddressException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/email/v1")
